e0aeedbf6d3b93fde682d27397133ebfdbab5acc
SNagaRakeshh <nagarakeshsamudrala@gmail.com>
2026-05-28 13:49:04 +0530

    chore: initialize Spring Boot URL shortener project

---
+++ src/main/java/com/BEProjects/url_shortener/HomeController.java
@@ -0,0 +1,14 @@
+package com.BEProjects.url_shortener;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
